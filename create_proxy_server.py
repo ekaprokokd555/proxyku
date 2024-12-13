e71c0d5e3d9a70c9098385ef645cd6aa3b7166eb
ekaprokokd555 <akyahumaira_22304373@bm400.sch.id>
2024-12-13 14:56:12 +0700

    Update create_proxy_server.py

--- create_proxy_server.py
+++ create_proxy_server.py
@@ -18,7 +18,7 @@ def create_ec2_instance():
         MinCount=1,
         MaxCount=1,
         KeyName=key_name,
-        SecurityGroupIds=['sg-01504071d4ce0bc11'],  # Ganti dengan ID security group Anda
+        SecurityGroupIds=['sg-0bfa02e88af4a6189'],  # Ganti dengan ID security group Anda
         UserData='''#!/bin/bash
                     sudo apt-get update -y
                     sudo apt-get install -y squid
