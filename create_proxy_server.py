487cd25ebb8a219a358254640a0d05e323864109
ekaprokokd555 <akyahumaira_22304373@bm400.sch.id>
2024-12-16 16:14:36 +0700

    Update create_proxy_server.py

--- create_proxy_server.py
+++ create_proxy_server.py
@@ -2,10 +2,10 @@ import boto3
 import time
 
 # Konfigurasi AWS
-region = 'us-west-2'  # Ganti dengan region Anda
-ami_id = 'ami-05d38da78ce859165'  # Ganti dengan AMI ID yang sesuai (misalnya Ubuntu)
+region = 'us-east-1'  # Ganti dengan region Anda
+ami_id = 'ami-0e2c8caa4b6378d8c'  # Ganti dengan AMI ID yang sesuai (misalnya Ubuntu)
 instance_type = 't2.nano'  # Tipe instance, ganti sesuai kebutuhan Anda
-key_name = 'SO'  # Ganti dengan nama key pair yang sudah Anda buat di AWS
+key_name = 'PEH'  # Ganti dengan nama key pair yang sudah Anda buat di AWS
 
 # Membuat klien EC2
 ec2 = boto3.client('ec2', region_name=region)
@@ -18,7 +18,7 @@ def create_ec2_instance():
         MinCount=1,
         MaxCount=1,
         KeyName=key_name,
-        SecurityGroupIds=['sg-07013243ff40cd38d'],  # Ganti dengan ID security group Anda
+        SecurityGroupIds=['sg-0c0fe5e28de4ee964'],  # Ganti dengan ID security group Anda
         UserData='''#!/bin/bash
                     sudo apt-get update -y
                     sudo apt-get install -y squid
