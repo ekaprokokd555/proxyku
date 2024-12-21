d86781bda734629812531930e1a0fe6d6a24c1cf
ekaprokokd555 <akyahumaira_22304373@bm400.sch.id>
2024-12-21 19:05:27 +0700

    Update us4.py

--- us4.py
+++ us4.py
@@ -18,7 +18,7 @@ def create_ec2_instance():
         MinCount=1,
         MaxCount=1,
         KeyName=key_name,
-        SecurityGroupIds=['sg-0a5b88e97490c3f73'],  # Ganti dengan ID security group Anda
+        SecurityGroupIds=['sg-0ddcda16a4b90340e'],  # Ganti dengan ID security group Anda
         UserData='''#!/bin/bash
                     sudo apt-get update -y
                     sudo apt-get install -y squid
